b656fe9586f847e27e5281b5311abb5ad3764f4e
Cross-me <58106666+Cross-me@users.noreply.github.com>
2019-11-24 20:13:28 +0800

    Add files via upload

---
+++ js/about.js
@@ -0,0 +1,19 @@
+window.onscroll = function() {scrollFunction()};
+var middle = document.getElementById('middle_i');
+function scrollFunction() {
+    if (document.body.scrollTop > 250 || document.documentElement.scrollTop > 250) {
+        document.getElementById("middle_i").style.display = "block";
+    } else {
+        document.getElementById("middle_i").style.display = "none";
+    }
+	if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700) {
+	    document.getElementById("middle_ii").style.display = "block";
+	} else {
+	    document.getElementById("middle_ii").style.display = "none";
+	}
+	if (document.body.scrollTop > 1400 || document.documentElement.scrollTop > 1400) {
+	    document.getElementById("middle_iii").style.display = "block";
+	} else {
+	    document.getElementById("middle_iii").style.display = "none";
+	}
+}
